04af4eaefa5eaeaf0ae3c4e7a55713f4e22a8d8b
Milos Jovanovic <milosj.mail@gmail.com>
2023-08-10 13:25:37 +0200

    Implemented slider
    Added slider for tasks so they don't overflow threw the container div

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { List } from 'src/app/models/list.model';
 import { Task } from 'src/app/models/task.model';
@@ -14,7 +14,8 @@ export class TaskViewComponent implements OnInit {
   lists: any;
   tasks: any;
   selectedListId: any;
-  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+  
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private elRef: ElementRef) { }
 
   ngOnInit() {
     console.log('TaskViewComponent initialized');
@@ -27,6 +28,15 @@ export class TaskViewComponent implements OnInit {
         this.taskService.getTasks(this.selectedListId).subscribe((tasks: any) => {
           this.tasks = tasks;
           console.log('Fetched tasks:', this.tasks);
+
+          const taskListContainer = this.elRef.nativeElement.querySelector('.task-list');
+      if (taskListContainer) {
+        const taskCount = tasks.length;
+        const taskHeight = 500; // Adjust the task height as needed
+        const maxContainerHeight = 500; // Adjust the maximum container height as needed
+        const calculatedHeight = Math.min(taskCount * taskHeight, maxContainerHeight);
+        taskListContainer.style.maxHeight = calculatedHeight + 'px';
+      }
         });
       }
   
